cd1bae3fcde20213439823340489492b807e082d
telvarost <telontelvarost@gmail.com>
2024-10-02 22:35:05 -0400

    Update biome spawns

--- src/main/java/com/matthewperiut/elementalcreepers/entity/BiomeModificationListener.java
+++ src/main/java/com/matthewperiut/elementalcreepers/entity/BiomeModificationListener.java
@@ -12,17 +12,21 @@ public class BiomeModificationListener {
     @EventListener
     public void event(BiomeModificationEvent event) {
         if (true == ElementalCreepersMod.config.biomeSpecific) {
-            Biome.SAVANNA.addHostileEntity(CookieCreeper.class, ElementalCreepersMod.config.cookieCreeperSpawn);
-            Biome.PLAINS.addHostileEntity(EarthCreeper.class, ElementalCreepersMod.config.earthCreeperSpawn);
-            Biome.DESERT.addHostileEntity(ElectricCreeper.class, ElementalCreepersMod.config.electricCreeperSpawn);
-            Biome.PLAINS.addHostileEntity(FireCreeper.class, ElementalCreepersMod.config.fireCreeperSpawn);
-            Biome.TAIGA.addHostileEntity(IceCreeper.class, ElementalCreepersMod.config.iceCreeperSpawn);
-            Biome.TUNDRA.addHostileEntity(IceCreeper.class, ElementalCreepersMod.config.iceCreeperSpawn);
-            Biome.ICE_DESERT.addHostileEntity(IceCreeper.class, ElementalCreepersMod.config.iceCreeperSpawn);
-            Biome.NETHER.addHostileEntity(MagmaCreeper.class, ElementalCreepersMod.config.magmaCreeperSpawn);
+            Biome.SEASONAL_FOREST.addHostileEntity(PsychicCreeper.class, ElementalCreepersMod.config.psychicCreeperSpawn);
             Biome.FOREST.addHostileEntity(PsychicCreeper.class, ElementalCreepersMod.config.psychicCreeperSpawn);
             Biome.SWAMPLAND.addHostileEntity(WaterCreeper.class, ElementalCreepersMod.config.waterCreeperSpawn);
             Biome.RAINFOREST.addHostileEntity(WaterCreeper.class, ElementalCreepersMod.config.waterCreeperSpawn);
+            Biome.RAINFOREST.addHostileEntity(ElectricCreeper.class, ElementalCreepersMod.config.electricCreeperSpawn);
+            Biome.SHRUBLAND.addHostileEntity(ElectricCreeper.class, ElementalCreepersMod.config.electricCreeperSpawn);
+            Biome.SKY.addHostileEntity(ElectricCreeper.class, ElementalCreepersMod.config.electricCreeperSpawn);
+            Biome.SAVANNA.addHostileEntity(CookieCreeper.class, ElementalCreepersMod.config.cookieCreeperSpawn);
+            Biome.PLAINS.addHostileEntity(EarthCreeper.class, ElementalCreepersMod.config.earthCreeperSpawn);
+            Biome.TAIGA.addHostileEntity(IceCreeper.class, ElementalCreepersMod.config.iceCreeperSpawn);
+            Biome.TUNDRA.addHostileEntity(IceCreeper.class, ElementalCreepersMod.config.iceCreeperSpawn);
+            Biome.ICE_DESERT.addHostileEntity(IceCreeper.class, ElementalCreepersMod.config.iceCreeperSpawn);
+            Biome.DESERT.addHostileEntity(FireCreeper.class, ElementalCreepersMod.config.fireCreeperSpawn);
+            Biome.NETHER.addHostileEntity(FireCreeper.class, ElementalCreepersMod.config.fireCreeperSpawn);
+            Biome.NETHER.addHostileEntity(MagmaCreeper.class, ElementalCreepersMod.config.magmaCreeperSpawn);
         }
     }
 }
